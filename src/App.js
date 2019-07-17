57f540a5c38f08314f264cc5c7fa4fc3d6b09552
YeoHoonYun <cjswo9207@gmail.com>
2019-07-17 21:48:50 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  NavLink,
+  Redirect
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Login from "./pages/Login";
+
+const activeStyle = {
+  color: "green"
+};
+
+const isLogin = true;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ul>
+        <li>
+          <NavLink exact to="/" activeStyle={activeStyle}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact to="/profile" activeStyle={activeStyle}>
+            Profile
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/profile/1" activeStyle={activeStyle}>
+            Profile/1
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about"
+            activeStyle={activeStyle}
+            isActive={(match, location) => {
+              if (location.pathname !== "/about") return false;
+              const searchParams = new URLSearchParams(location.search);
+              return !searchParams.has("name");
+            }}
+          >
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about?name=mark"
+            activeStyle={activeStyle}
+            isActive={(match, location) => {
+              if (location.pathname !== "/about") return false;
+              const searchParams = new URLSearchParams(location.search);
+              return searchParams.has("name");
+            }}
+          >
+            About?name=mark
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" activeStyle={activeStyle}>
+            Login
+          </NavLink>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/profile/:id" component={Profile} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/about" component={About} />
+        <Route
+          path="/login"
+          render={() => (isLogin ? <Redirect to="/" /> : <Login />)}
+        />
+        <Route path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
